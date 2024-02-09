3157913e8e831afcc1a49d7a523351defd41a9fb
dhruvi6071 <dhruvi6071@gmail.com>
2024-02-09 17:26:28 +0530

    ref-use

---
+++ 01-starting-project/src/components/Player.jsx
@@ -0,0 +1,27 @@
+import { useState, useRef } from "react";
+
+export default function Player() {
+  const realPlayerName = useRef();
+  const [playerName, setPlayerName] = useState(null);
+  // const [submitted, setSubmitted] = useState(false);
+
+  // function handleChange(event){
+  //    setSubmitted(false);
+  //   setPlayerName(event.target.value);
+  // }
+
+  function handleClick(){
+    setPlayerName(realPlayerName.current.value);
+  }
+
+  return (
+    <section id="player">
+      {/* <h2>Welcome {submitted ? playerName : 'unknown entity'}</h2> */}
+      <h2>Welcome {playerName ?? 'unknown entity'}</h2>
+      <p>
+        <input type="text" ref={realPlayerName}/>
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
