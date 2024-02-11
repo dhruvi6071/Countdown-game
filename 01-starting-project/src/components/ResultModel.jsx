db816b3b3ad36a9e34a985cded7209484fc93638
dhruvi6071 <dhruvi6071@gmail.com>
2024-02-11 17:36:57 +0530

    final-project

--- 01-starting-project/src/components/ResultModel.jsx
+++ 01-starting-project/src/components/ResultModel.jsx
@@ -1,11 +1,17 @@
-import { forwardRef, useImperativeHandle, useRef } from "react";
+// useImperativeHandle(first prop must be the ref by forwardref, second prop should be a function which contains all the properties that need to be exposed )
+ 
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
-const ResultModel = forwardRef(function ResultModel(
-  { result, targetTime },
+const ResultModal = forwardRef(function ResultModal(
+  { targetTime, remainingTime, onReset },
   ref
 ) {
-  // useImperativeHandle(first prop must be the ref by forwardref, second prop should be a function which contains all the properties that need to be exposed )
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -15,20 +21,23 @@ const ResultModel = forwardRef(function ResultModel(
     };
   });
 
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
-        The target time was <strong>{targetTime}</strong>
+        The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{' '}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
   );
 });
 
-export default ResultModel;
+export default ResultModal;
